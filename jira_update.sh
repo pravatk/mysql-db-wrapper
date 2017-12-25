da9ce37eabf935087da2adb2dd81d5e6f22405ba
pravatk <sahoopravatk@gmail.com>
2017-12-25 12:03:42 +0530

    Fixing jira update script

--- jira_update.sh
+++ jira_update.sh
@@ -2,4 +2,4 @@ echo $JIRA_USERNAME
 echo $JIRA_PASSWORD
 echo $JIRA_TICKET
 
-curl -D- -u $JIRA_USERNAME:$JIRA_PASSWORD -X POST -H "X-Atlassian-Token: nocheck" -F "file=@coverage.zip" https:/kickdrum.atlassian.net/rest/api/2/issue/$JIRA_TICKET/attachments
+curl -D- -u $JIRA_USERNAME:$JIRA_PASSWORD -X POST -H "X-Atlassian-Token: nocheck" -F "file=@coverage.zip" https://kickdrum.atlassian.net/rest/api/2/issue/$JIRA_TICKET/attachments
